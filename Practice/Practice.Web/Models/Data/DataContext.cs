07561e086b9c790bd6abcafc340009fe4e16aa4d
fernando <lucumicruzhector@gmail.com>
2021-06-22 07:24:13 -0500

    Adding Entity Product, Model and Products Controller

---
+++ Practice/Practice.Web/Models/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Practice.Web.Models.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using Practice.Web.Models.Entities;
+
+
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ Practice/Practice.Web/Models/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Practice.Web.Models.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product : IEntity
+    {
+    
+    public int Id { get; set; }
+    [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters length!")]
+    [Required]
+    public string Name { get; set; }
+    [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+    public decimal Price { get; set; }
+    [Display(Name = "Image")]
+    public string ImageUrl { get; set; }
+    [Display(Name = "Last Purchase")]
+    public DateTime? LastPurchase { get; set; }
+    [Display(Name = "Last Sale")]
+    public DateTime? LastSale { get; set; }
+    [Display(Name = "Is Available ?")]
+    public bool IsAvailable { get; set; }
+    [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+    public double Stock { get; set; }
+    public User User { get; set; }
+
+    }
+}
